057c847988c022f5824b82b3cf1f45ba5208a872
Cameron Hurst <cameron_hurst@rocketmail.com>
2019-05-10 12:44:12 -0600

    checkpoint 2 project

---
+++ pageObjects/react.js
@@ -0,0 +1,26 @@
+module.exports = {
+    url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
+    elements: {
+        eo: 'input[name="evenOddInput"]',
+        split: 'button[name="evenOddButton"]',
+        evens: '[name="evenResults"]',
+        odds: '[name="oddResults"]',
+
+        fo: '[name="objectFilterInput"]',
+        filtero: '[name="objectFilterButton"]',
+        filteredo: '[name="objectFilterResults"]',
+
+        fs: '#nameFilterInput',
+        filters: '#nameFilterButton',
+        filtereds: '[name="nameFilterResults"]',
+
+        pd: '[name="palindromeInput"]',
+        check: '[name="palindromeButton"]',
+        palindrome: '[name="palindromeResults"]',
+
+        sm1: '[name="sumInput1"]',
+        sm2: '[name="sumInput2"]',
+        add: '[name="sumButton"]',
+        sum: '[name="sumResults"]',
+    }
+}
